6fefd6b75c2048fd929b5c92f6015699df492136
Xinxin Chen <xinxinchen476@gmail.com>
2025-10-26 17:14:17 -0400

    modified comments

--- survival_analysis/code/01_grid_setup.R
+++ survival_analysis/code/01_grid_setup.R
@@ -1,4 +1,8 @@
-# Set up a grid of model and prior combinations for running the real data analysis on the computing cluster
+## -----------------------------------------------------------------------------
+## Set up a grid of model and prior combinations for running the real data analysis
+## on the computing cluster.
+## -----------------------------------------------------------------------------
+
 library(dplyr)
 
 # Directory to save the grid
@@ -7,11 +11,11 @@ save.dir <- 'code'
 # Specify models and priors to be evaluated in the analysis
 # "PWE" corresponds to the PWEPH model and "CurePWE" to the CurePWEPH model in the manuscript
 model <- c("PWE", "CurePWE")
-# The analysis compares multiple prior specifications, including the vague prior ("ref"), power prior ("pp"),
-# propensity score–integrated power prior ("psipp"), Bayesian hierarchical model ("bhm"),
+# The analysis compares multiple prior specifications, including the vague prior ("ref"), 
+# power prior ("pp"), propensity score–integrated power prior ("psipp"), Bayesian hierarchical model ("bhm"),
 # commensurate prior ("cp"), latent exchangeability prior ("leap"), and normalized power prior ("npp").
 prior <- c('ref', 'pp', 'psipp', 'bhm', 'cp', 'leap', 'npp')
-# Set the number of intervals (J) for the PWEPH and CurePWEPH model.
+# Set the number of intervals (J) for the PWEPH and CurePWEPH model
 # We use values from 2 to 9. The upper limit (J = 9) ensures that each interval contains at least one event
 # in both the stratified current and external data sets.
 J      <- 2:9
